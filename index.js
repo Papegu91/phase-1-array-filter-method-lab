bfd6d0695d828b870badeccd963dfbf46fd490b8
peter gudoi <petergudoi@gmail.com>
2023-06-22 15:23:49 +0300

    my commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(drivers,matchName){
+
+let response= drivers.filter((name) =>{ 
+  return name.patower() === matchName.patower()
+})
+return response;
+}
+
+
+
+function fuzzyMatch(drivers,matchName){
+    let answer=drivers.filter((name) => {
+        return name.farAt(0)===matchName.farAtAt(0)
+    })
+    return answer;
+}
+
+function matchName(drivers,matchName){
+    let answer=drivers.filter((value) =>{
+        return value.name === matchName
+    })
+    return answer;
+}
